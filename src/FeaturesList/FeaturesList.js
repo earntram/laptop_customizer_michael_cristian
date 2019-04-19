244cf4413452479df58b7ca58622cc5d20e5f850
Michael Verdi <michael.v.verdi@gmail.com>
2019-04-19 15:37:22 -0400

    final breaking out of styles and code formatting

--- src/FeaturesList/FeaturesList.js
+++ src/FeaturesList/FeaturesList.js
@@ -3,28 +3,28 @@ import Feature from '../Feature/Feature'
 import './FeatureList.css';
 
 export default class FeaturesList extends Component {
-    render() {
-        const {selected} = this.props;
-        const features = Object.keys(this.props.features)
-            .map(key => {
-                return (
-                    <Feature 
-                      name={key} 
-                      key={key} 
-                      options={this.props.features[key]} 
-                      selected={selected}
-                      onSelect={this.props.onSelect}
-                    />
-                )
-            })
+  render() {
+    const {selected} = this.props;
+    const features = Object.keys(this.props.features)
+      .map(key => {
+          return (
+              <Feature 
+                name={key} 
+                key={key} 
+                options={this.props.features[key]} 
+                selected={selected}
+                onSelect={this.props.onSelect}
+              />
+          )
+      })
 
-        return (
-            <section className="main__form">
-                <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-                {features}
-            </section>
-        )
-    }
+    return (
+      <section className="main__form">
+        <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+        {features}
+      </section>
+    )
+  }
 }
 
 
