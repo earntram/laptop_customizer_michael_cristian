5f05349b081f1af25ccf97a13b4d36e3ce922e05
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-04-19 12:04:42 -0700

    featureOption bug

--- src/FeaturesList/FeaturesList.js
+++ src/FeaturesList/FeaturesList.js
@@ -1,38 +1,27 @@
 import React, {Component} from 'react'
+import Feature from '../Feature/Feature'
 
 export default class FeaturesList extends Component {
     render() {
         const {selected} = this.props;
 
         const features = Object.keys(this.props.features)
-        .map(key => {
-          const options = this.props.features[key].map((item, index) => {
-            const selectedClass = item.name === selected[key].name ? 'feature__selected' : '';
-            const featureClass = 'feature__option ' + selectedClass;
-            return <li key={index} className="feature__item">
-              <div className={featureClass}
-                
-                onClick={e => this.props.onSelect(key, item)}>
-                  { item.name }
-                  ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                    .format(item.cost) })
-              </div>
-            </li>
-          });
-
-          return <div className="feature" key={key}>
-            <div className="feature__name">{key}</div>
-            <ul className="feature__list">
-              { options }
-            </ul>
-          </div>
-        });      
+            .map(key => {
+                return (
+                    <Feature 
+                    name={key} 
+                    key={key} 
+                    options={this.props.features[key]} 
+                    selected={selected}
+                    onSelect={this.props.onSelect} />
+                )
+            })
 
         return (
-        <section className="main__form">
-            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-            { features }
-        </section>
+            <section className="main__form">
+                <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+                {features}
+            </section>
         )
     }
 }
